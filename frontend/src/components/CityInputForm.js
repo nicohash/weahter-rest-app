dbe2c3c5c15d1c82912fb727a685f8a5c46d1454
nicohash <58517269+nicohash@users.noreply.github.com>
2023-06-18 00:44:27 +0200

    adds docs

--- frontend/src/components/CityInputForm.js
+++ frontend/src/components/CityInputForm.js
@@ -1,6 +1,12 @@
 import React, { useRef } from "react";
 import styles from "./CityInputForm.module.css";
 
+/**
+ * This is a custom input form that takes the input of the city name from the user which is named inputCity
+ * @generator
+ * @param {PropertyDefinition} props 
+ * @returns {JSX}
+ */
 const CityInputForm = (props) => {
   const cityInputRef = useRef();
 
